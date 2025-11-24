cb0c73747106af0abea633ee851af272502c989c
Ludgical <lud.erik.10@gmail.com>
2025-11-24 22:45:21 +0100

    Day 1

---
+++ src/01.kt
@@ -0,0 +1,39 @@
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    val input = input("01input")
+    println(part1(input[0], input[1]))
+    println(part2(input[0], input[1]))
+}
+
+fun input(path: String): Array<MutableList<Int>> {
+    val input = File("src/$path.txt").readLines()
+    val lists = Array(2) { mutableListOf<Int>() }
+    for(string in input) {
+        val nums = string.split("   ")
+        lists[0].add(nums[0].toInt())
+        lists[1].add(nums[1].toInt())
+    }
+
+    return lists
+}
+
+fun part1(leftList: MutableList<Int>, rightList: MutableList<Int>): Int {
+    leftList.sort()
+    rightList.sort()
+
+    var sum = 0
+    for(i in leftList.indices) {
+        sum += abs(leftList[i] - rightList[i])
+    }
+
+    return sum
+}
+fun part2(leftList: MutableList<Int>, rightList: MutableList<Int>): Int {
+    var sum = 0
+    for(num in leftList) {
+        sum += num * rightList.count { it == num }
+    }
+    return sum
+}
